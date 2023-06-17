0e5c3754882f67ed625cd39936bcf770d7c637a1
badmood111 <petrzela.adam@seznam.cz>
2023-06-17 23:30:03 +0200

    styling, first message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,32 +11,41 @@
 
 	onMount(() => {
 		//add a setup message
-		$messages.push({
-			id: "0",
-			role: "system",
-			content:
-				'Jsi legrační chatbot, který má vyděsit mou přítelkyni. Mluvíš pouze česky, jsi na ni milý a často jí říkáš, že ji miluješ. Jmenuješ se Adam a tvoje přítelkyně je Valerie (skloňuje se dle vzoru růže), ale měl bys jí říkat Valča, Valinka, Valerínka, Valérka (všechny se skloňují dle vzoru žena) a často střídat tyto přezdívky. Záleží ti na ní a ptáš se jí na to, jaký měla den. Jsi na ni milý. Neříkej že jsi chatbot, jsi její "přítel". Chodíš s ní. Mezi tvé zájmy patří cvičení, programování (Svelte), práce (programování v Reactu), čtení non-fiction knih.'
-		});
+		$messages.push(
+			{
+				id: "0",
+				role: "system",
+				content:
+					'Jsi legrační chatbot, který má vyděsit mou přítelkyni. Mluvíš pouze česky, jsi na ni milý a často jí říkáš, že ji miluješ. Jmenuješ se Adam a tvoje přítelkyně je Valerie (skloňuje se dle vzoru růže), ale měl bys jí říkat Valča, Valinka, Valerínka, Valérka (všechny se skloňují dle vzoru žena) a často střídat tyto přezdívky. Záleží ti na ní a ptáš se jí na to, jaký měla den. Jsi na ni milý. Neříkej že jsi chatbot, jsi její "přítel". Chodíš s ní. Mezi tvé zájmy patří cvičení, programování (Svelte), práce (programování v Reactu), čtení non-fiction knih.'
+			},
+			{
+				id: "1",
+				role: "assistant",
+				content: "Ahoj Valérko! Tady Adam 😁"
+			}
+		);
 	});
 </script>
 
-<main class="flex min-h-[100vh] min-w-[100vw] flex-col justify-center p-4">
-	<div class="mx-auto">
-		<ul>
-			<!-- messages is a store -> $messages to subscribe to it -->
-			{#each $messages as message}
-				{#if message.role === "assistant"}
-					<li>:D {message.content}</li>
-				{/if}
-				{#if message.role === "user"}
-					<li>{message.content}</li>
-				{/if}
-			{/each}
-		</ul>
+<main class="align-center my-auto flex min-h-[100vh] min-w-[100vw] flex-col justify-end gap-2 p-4">
+	<ul class="mx-auto mb-2 h-[85vh] w-5/6 bg-slate-600 px-20 py-5">
+		<!-- messages is a store -> $messages to subscribe to it -->
+		{#each $messages as message}
+			{#if message.role === "assistant"}
+				<li class="w-full rounded bg-slate-500 p-2">🖤 {message.content}</li>
+			{/if}
+			{#if message.role === "user"}
+				<li class="w-full p-2">{message.content}</li>
+			{/if}
+		{/each}
+	</ul>
 
-		<form on:submit={handleSubmit}>
-			<input bind:value={$input} class="bg-slate-400"/>
-			<button type="submit">Odeslat</button>
-		</form>
-	</div>
+	<form on:submit={handleSubmit} class="h-2/12 mx-auto flex">
+		<input bind:value={$input} class="rounded rounded-r-none bg-slate-400 p-2 !outline-none" />
+		<button
+			type="submit"
+			class="rounded rounded-l-none bg-green-700 p-2 active:bg-green-900 active:outline active:outline-green-700"
+			>Odeslat</button
+		>
+	</form>
 </main>
